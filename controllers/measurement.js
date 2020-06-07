7886131d19caf901ac5f246d3c6463b4a90a0f8f
JaysusM <jmartinruiz@uma.es>
2020-06-07 13:50:52 +0200

    Add query filter for measurements

--- controllers/measurement.js
+++ controllers/measurement.js
@@ -1,37 +1,50 @@
-const Measurement = require('../models/measurement');
-const localeManager = require('../utils/locales/localeManager');
+const Measurement = require("../models/measurement");
+const localeManager = require("../utils/locales/localeManager");
 
 exports.new = (req, res) => {
-    const measurement = {
-        userId: req.body.userId,
-        position: req.body.position,
-        value: req.body.value
+  const measurement = {
+    userId: req.body.userId,
+    position: req.body.position,
+    value: req.body.value,
+  };
+
+  const locale = localeManager(req.body.language);
+  const measurementLocale = locale.MEASUREMENT.NEW;
+
+  if (!measurement.userId || !measurement.position || !measurement.value) {
+    res.json(measurementLocale.EMPTY_FIELDS);
+  }
+
+  measurement.date = new Date();
+
+  Measurement.create(measurement).then((newMeasurement) => {
+    if (!newMeasurement) {
+      res.json(locale.GENERAL.COMMON_ERROR);
+    } else {
+      res.json(measurementLocale.SUCCESS);
     }
-
-    const locale = localeManager(req.body.language);
-    const measurementLocale = locale.MEASUREMENT.NEW;
-
-    if (!measurement.userId || !measurement.position || !measurement.value) {
-        res.json(measurementLocale.EMPTY_FIELDS);
-    }
-
-    measurement.date = new Date();
-
-    Measurement
-        .create(measurement)
-        .then(newMeasurement => {
-            if (!newMeasurement) {
-                res.json(locale.GENERAL.COMMON_ERROR);
-            } else {
-                res.json(measurementLocale.SUCCESS);
-            }
-        })
+  });
 };
 
 exports.list = (req, res) => {
-    Measurement
-        .find({})
-        .then(measurements => {
-            res.json(measurements);
-        })
+  let from = req.query.from;
+  let to = req.query.to;
+
+  let date = {};
+
+  if (from) {
+    date = {date: {}}
+    date.date["$gte"] = from;
+  }
+
+  if (to) {
+    if (!date.date) {
+        date = {date: {}}
+    }
+    date.date["$lte"] = to;
+  } 
+
+  Measurement.find(date).then((measurements) => {
+    res.json(measurements);
+  });
 };
